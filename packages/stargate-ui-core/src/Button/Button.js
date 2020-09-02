93f8e058740da928cd9b06a47f09db43462791f0
Keit Oliveira <keitoliveira@outlook.com>
2020-09-01 21:03:14 -0300

    refactor(core): add variants for Button

--- packages/stargate-ui-core/src/Button/Button.js
+++ packages/stargate-ui-core/src/Button/Button.js
@@ -2,32 +2,66 @@ import React from 'react';
 import { useStyles } from '@pontte/stargate-ui-styles';
 import clsx from 'clsx';
 
-const styles = ({ spacing }) => {
-  const base = {
+import Factory from '../Factory';
+import Typography from '../Typography';
+
+const styles = (theme) => {
+  const {
+    spacing,
+    palette,
+    radius,
+  } = theme;
+
+  const button = {
     display: 'inline-flex',
-    padding: [spacing(), spacing()],
-    border: 0,
-    borderRadius: spacing(2),
+    padding: [spacing(), spacing(2)],
+    border: [[1, 'solid', 'transparent']],
+    borderRadius: radius(10),
     cursor: 'pointer',
     outline: 'none',
-    '&:hover': {
-      opacity: .5,
+  };
+
+  const buttonCommon = {
+    color: palette.lighter,
+  };
+
+  const buttonColors = {
+    primary: {
+      ...buttonCommon,
+      backgroundColor: palette.primary[0],
+      borderColor: palette.primary[0],
+    },
+    secondary: {
+      ...buttonCommon,
+      color: palette.text[0],
+      backgroundColor: palette.secondary[0],
+      borderColor: palette.secondary[0],
     },
   };
 
   return {
-    base,
+    button,
+    ...buttonColors,
   };
 };
 
-const Button = ({ children }) => {
+const Button = (props) => {
+  const {
+    children,
+    color = '',
+    variant = 'contained',
+    ...factoryProps
+  } = props;
+
   const classes = useStyles(styles);
-  const className = clsx(classes.base);
+  const className = clsx(classes.button, classes[color]);
 
   return (
-    <button type="button" className={className}>
-      {children}
-    </button>
+    <Factory type="button" className={className} {...factoryProps}>
+      <Typography type="span">
+        {children}
+      </Typography>
+    </Factory>
   );
 };
 
