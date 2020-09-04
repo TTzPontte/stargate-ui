b90e5693e2ed8a25046f1398c6df2de3d0242321
Keit Oliveira <keitoliveira@outlook.com>
2020-09-04 18:30:26 -0300

    chore(core): add boilerplate

---
+++ packages/stargate-ui-core/src/.boilerplate/Component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useStyles } from '@pontte/stargate-ui-styles';
+import clsx from 'clsx';
+
+import Factory from '../Factory';
+
+const styles = () => {
+  return {};
+};
+
+const Component = (props) => {
+  const { children, ...factoryProps } = props;
+
+  const classes = useStyles(styles, {});
+  const className = clsx(classes);
+
+  return (
+    <Factory className={className} {...factoryProps}>
+      {children}
+    </Factory>
+  );
+};
+
+export default Component;
