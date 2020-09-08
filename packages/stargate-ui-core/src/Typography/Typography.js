16ea1f4b137b8d7786b92de42822776b1b6410da
keitoliveira <keitoliveira@outlook.com>
2020-09-08 16:10:33 -0300

    fix(core): add correctly exports

--- packages/stargate-ui-core/src/Typography/Typography.js
+++ packages/stargate-ui-core/src/Typography/Typography.js
@@ -1,4 +1,4 @@
-import React, { createElement } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useStyles } from '@pontte/stargate-ui-styles';
 import clsx from 'clsx';
