f25a41d25e50fa72632164c76cb555b819fed036
keitoliveira <keitoliveira@outlook.com>
2020-09-08 16:09:24 -0300

    build(core): add PostCss plugin and global definitions

--- packages/stargate-ui-core/rollup.config.js
+++ packages/stargate-ui-core/rollup.config.js
@@ -1,48 +1,73 @@
 import path from 'path';
 import dotenv from 'dotenv';
-import resolve from 'rollup-plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import image from 'rollup-plugin-img';
-import replace from 'rollup-plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import replace from '@rollup/plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
 dotenv.config();
 
 const { NODE_ENV } = process.env;
 const env = { NODE_ENV };
 
-const config = [
-  {
-    input: 'src/index.js',
-    output: {
-      dir: 'dist',
-      format: 'cjs',
-      name: 'stargate-ui',
-    },
-    external: [
-      'react',
-      'react-dom',
-      'prop-types',
-      'react-is',
-    ],
-    plugins: [
-      image(),
-      resolve({ browser: true }),
-      replace(
-        Object.keys(env).reduce((props, name) => ({
-          ...props,
-          [name]: JSON.stringify(env[name]),
-        }), {}),
-      ),
-      babel({
-        configFile: path.resolve(__dirname, 'babel.config.js'),
-        babelHelpers: 'runtime',
-      }),
-      commonjs({
-        include: /node_modules/,
-      }),
-    ],
+/**
+ * @todo need to check all external dependencies
+ */
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'react-is': 'ReactIs',
+  'object-assign': 'Object.assign',
+  '@pontte/stargate-ui-styles': 'StargateUIStyles',
+};
+
+const external = Object.keys(globals);
+
+const production = {
+  external,
+  input: 'src/index.js',
+  output: {
+    globals,
+    format: 'umd',
+    file: 'dist/stargate-ui-core.min.js',
+    name: 'StargateUICore',
   },
-];
+  plugins: [
+    postcss({ extract: true, extensions: ['.css'] }),
+    image(),
+    nodeResolve({
+      /**
+       * @todo add in another config file
+       */
+      extensions: [
+        '.js',
+        '.jsx',
+        '.json',
+      ],
+    }),
+    commonjs({ ignoreGlobal: true,
+      include: /node_modules/,
+    }),
+    babel({
+      configFile: path.resolve(__dirname, 'babel.config.js'),
+      babelHelpers: 'runtime',
+    }),
+    replace(
+      Object.keys(env).reduce((props, name) => ({
+        ...props,
+        [name]: JSON.stringify(env[name]),
+      }), {}),
+    ),
+    terser(),
+  ],
+};
+
+/**
+ * @todo create bundle for development
+ */
+const config = [production];
 
 export default config;
