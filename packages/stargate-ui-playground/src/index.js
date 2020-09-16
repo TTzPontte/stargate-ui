de02f427e4a89936f6a73fabca717dd59b761251
keitoliveira <keitoliveira@outlook.com>
2020-09-16 18:37:48 -0300

    feat(playground): add Link

--- packages/stargate-ui-playground/src/index.js
+++ packages/stargate-ui-playground/src/index.js
@@ -1,4 +1,5 @@
 export * from '@storybook/addon-docs/blocks';
+export { default as Link } from '@storybook/addon-links/react';
 export { default as Palette } from './Palette';
 export { default as Swatch } from './Swatch';
 export { default as Status } from './Status';
