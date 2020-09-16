e255ca40a5bd955b1f746fa1eee16110f4aaa560
saile marinho <saile-ribeiro@hotmail.com>
2020-09-16 19:18:48 -0300

    feat(core): add Checkbox initial configs

--- packages/stargate-ui-core/src/Checkbox/Checkbox.js
+++ packages/stargate-ui-core/src/Checkbox/Checkbox.js
@@ -1,24 +1,85 @@
+import PropTypes from "prop-types";
 import React from 'react';
 import { useStyles } from '@pontte/stargate-ui-styles';
 import clsx from 'clsx';
 
 import Factory from '../Factory';
 
-const styles = () => {
-  return {};
+const styles = (theme) => {
+  const {
+    palette,
+    radius,
+    spacing,
+    border,
+    active,
+    mode,
+  } = theme;
+
+  const getColor = (color) => (
+    palette?.[color][mode].color
+  );
+
+  const checkbox = {
+    width: '100%',
+    border: 0,
+    borderRadius: '50%',
+    backgroundColor: (props) => {
+      const {
+        disabled,
+        readonly,
+        color,
+      } = props;
+
+      return (disabled || readonly) && setLightness(.95, getColor(color));
+    },
+  };
+
+
+  return {checkbox};
 };
 
 const Checkbox = (props) => {
-  const { children, ...factoryProps } = props;
+  const {
+    disabled,
+    readonly,
+    label,
+    color = 'default',
+    onChange = () => {},
+    ...factoryProps
+  } = props;
 
-  const [classes] = useStyles(styles, {});
-  const className = clsx(classes);
+  const [
+    {
+      checkbox: classCheckbox,
+      ...classes
+    }
+  ] = useStyles(styles, {
+    disabled,
+    readonly,
+    color,
+  });
+  const className = clsx(Object.values(classes));
+  console.log(classCheckbox)
 
   return (
-    <Factory type="input" className={className} {...factoryProps}>
-      {children}
-    </Factory>
+      <Factory
+        element="input"
+        type="checkbox"
+        className={classCheckbox}
+        disabled={disabled}
+        readOnly={readonly}
+        {...factoryProps}
+      />
   );
 };
+
+Checkbox.propTypes = {
+  color: PropTypes.string,
+  disabled: PropTypes.any,
+  label: PropTypes.any,
+  onChange: PropTypes.func,
+  readonly: PropTypes.any,
+  type: PropTypes.string
+}
 
 export default Checkbox;
