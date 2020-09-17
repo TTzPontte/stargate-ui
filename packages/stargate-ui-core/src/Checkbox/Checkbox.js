03b1e2ac71f2c922aea86e7cb793fc9206b568be
keitoliveira <keitoliveira@outlook.com>
2020-09-17 14:23:19 -0300

    fix(core): add initial structure for Checkbox

--- packages/stargate-ui-core/src/Checkbox/Checkbox.js
+++ packages/stargate-ui-core/src/Checkbox/Checkbox.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from 'react';
 import { useStyles } from '@pontte/stargate-ui-styles';
+import { InputLabel } from '@pontte/stargate-ui-core';
 import clsx from 'clsx';
 
 import Factory from '../Factory';
@@ -20,22 +21,42 @@ const styles = (theme) => {
   );
 
   const checkbox = {
-    width: '100%',
-    border: 0,
-    borderRadius: '50%',
-    backgroundColor: (props) => {
-      const {
-        disabled,
-        readonly,
-        color,
-      } = props;
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    userSelect: 'none',
+    opacity: 0,
+    '&:checked ~ $checkboxMark:after': {
+      borderColor: 'blue',
+    }
+    // width: '100%',
+    // border: 0,
+    // borderRadius: '50%',
+    // backgroundColor: (props) => {
+    //   const {
+    //     disabled,
+    //     readonly,
+    //     color,
+    //   } = props;
 
-      return (disabled || readonly) && setLightness(.95, getColor(color));
+    //   return (disabled || readonly) && setLightness(.95, getColor(color));
+    // },
+  };
+
+  const checkboxMark = {
+    '&:after': {
+      content: '""',
+      display: 'inline-flex',
+      width: 30,
+      height: 30,
+      border: [[1, 'solid', 'red']]
     },
   };
 
-
-  return {checkbox};
+  return {
+    checkbox,
+    checkboxMark,
+  };
 };
 
 const Checkbox = (props) => {
@@ -51,6 +72,7 @@ const Checkbox = (props) => {
   const [
     {
       checkbox: classCheckbox,
+      checkboxMark: classCheckboxMark,
       ...classes
     }
   ] = useStyles(styles, {
@@ -62,6 +84,7 @@ const Checkbox = (props) => {
   console.log(classCheckbox)
 
   return (
+    <InputLabel>
       <Factory
         element="input"
         type="checkbox"
@@ -70,13 +93,20 @@ const Checkbox = (props) => {
         readOnly={readonly}
         {...factoryProps}
       />
+      <Factory
+        element="span"
+        className={classCheckboxMark}
+      >
+        {label}
+      </Factory>
+    </InputLabel>
   );
 };
 
 Checkbox.propTypes = {
   color: PropTypes.string,
   disabled: PropTypes.any,
-  label: PropTypes.any,
+  label: PropTypes.any.isRequired,
   onChange: PropTypes.func,
   readonly: PropTypes.any,
   type: PropTypes.string
