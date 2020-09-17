84175625e951d51a74da67b9032a6d00cad8794c
keitoliveira <keitoliveira@outlook.com>
2020-09-16 21:17:51 -0300

    feat(icons): add initial icons

---
+++ packages/stargate-ui-icons/src/ArrowRight.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import createIcon from './utils/createIcon';
+
+export default createIcon(
+  'ArrowRight',
+  <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2">
+    <circle cx="13" cy="13" r="13" stroke-miterlimit="10"/>
+    <path d="M16 20l-6.79-5.44a2 2 0 010-3.12L16 6" stroke-linejoin="round" />
+  </g>
+);
---
+++ packages/stargate-ui-icons/src/utils/createIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SvgIcon } from '@pontte/stargate-ui-core';
+
+const createIconComponent = (name, path) => (
+  React.memo(
+    React.forwardRef((props, ref) => (
+      <SvgIcon
+        ref={ref}
+        name={name}
+        children={path}
+        {...props}
+      />
+    ))
+  )
+);
+
+export default createIconComponent;
