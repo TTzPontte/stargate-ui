35deaac64f21549933297d6413b3fc97795d6605
saile marinho <saile-ribeiro@hotmail.com>
2020-09-18 17:17:58 -0300

    feat(core): add Radio Button

--- packages/stargate-ui-core/src/Radio/Radio.js
+++ packages/stargate-ui-core/src/Radio/Radio.js
@@ -1,24 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
 import { useStyles } from '@pontte/stargate-ui-styles';
+import { InputLabel } from '@pontte/stargate-ui-core';
 import clsx from 'clsx';
 
 import Factory from '../Factory';
 
-const styles = () => {
-  return {};
+const styles = (theme) => {
+  const {
+    palette,
+    radius,
+    mode,
+  } = theme;
+
+  const { setLightness } = palette;
+  const getColor = (color) => (
+    palette?.[color][mode].color
+  );
+
+  const radio = {
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    userSelect: 'none',
+    opacity: 0,
+  };
+
+  const radioSelected = {
+    '&:after': {
+      content: '""',
+      display: 'inline-flex',
+      cursor: 'pointer',
+      width: 20,
+      height: 20,
+      borderRadius: radius(),
+      backgroundColor: palette.lighter,
+      border: ({color}) => ([[1, 'solid', getColor(color)]]),
+    },
+    '& ~ $radioSelected:selected': {
+      backgroundColor: (props) => {
+        const {
+          disabled,
+          readonly,
+          color,
+        } = props;
+
+        return (disabled || readonly) ? setLightness(.95, getColor(color)) : getColor(color)
+      },
+    }
+  };
+
+  return {
+    radio,
+    radioSelected,
+  };
 };
 
 const Radio = (props) => {
-  const { children, ...factoryProps } = props;
+  const {
+    disabled,
+    readonly,
+    label,
+    color = 'default',
+    selected: defaultValue = false,
+    onChange = () => {},
+    ...factoryProps
+  } = props;
 
-  const [classes] = useStyles(styles, {});
-  const className = clsx(classes);
+  const [
+    {
+      radio: classRadio,
+      radioSelected: classRadioSelected,
+      ...classes
+    }
+  ] = useStyles(styles, {
+    disabled,
+    readonly,
+    color,
+  });
+  const className = clsx(Object.values(classes));
+
+  const [selected, setSelected] = useState(defaultValue);
+
+  const handleClick = ({ currentTarget: { selected } }) => {
+    if (disabled) {
+      return;
+    }
+
+    setSelected(selected);
+    onClick({ selected });
+  }
 
   return (
-    <Factory type="input" className={className} {...factoryProps}>
-      {children}
-    </Factory>
+    <InputLabel>
+      <Factory
+        element="input"
+        type="radio"
+        className={classRadio}
+        />
+      <Factory
+        element="span"
+        className={classRadioSelected}
+        onClick={handleClick}
+        selected={selected}
+        marginRight={1}
+        {...factoryProps}
+        >
+      </Factory>
+      {label}
+    </InputLabel>
   );
 };
+
+Radio.propTypes = {
+  color: PropTypes.string,
+  disabled: PropTypes.bool,
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.node,
+  ]),
+  selected: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+  readonly: PropTypes.bool,
+  type: PropTypes.string
+}
 
 export default Radio;
