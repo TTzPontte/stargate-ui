7a40a7f5e7669bb03177f5f260d6c186941f3d4f
saile marinho <saile-ribeiro@hotmail.com>
2020-09-24 15:34:10 -0300

    feat(icons): add social media icons

--- packages/stargate-ui-icons/src/index.js
+++ packages/stargate-ui-icons/src/index.js
@@ -1,3 +1,8 @@
 export { default as ArrowLeft } from './ArrowLeft';
 export { default as ArrowRight } from './ArrowRight';
 export { default as Close } from './Close';
+export { default as Facebook } from './Facebook';
+export { default as Instagram } from './Instagram';
+export { default as Whatsapp } from './Whatsapp';
+export { default as Youtube } from './Youtube';
+export { default as Linkedin } from './Linkedin';
