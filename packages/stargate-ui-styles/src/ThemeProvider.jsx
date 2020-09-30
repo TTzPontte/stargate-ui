8aaf29fe8b2e730882565383df9888b9a89e978a
keitoliveira <keitoliveira@outlook.com>
2020-09-30 18:56:33 -0300

    fix(styles): add correctly order of providers and remove classname
    prefix

--- packages/stargate-ui-styles/src/ThemeProvider.jsx
+++ packages/stargate-ui-styles/src/ThemeProvider.jsx
@@ -1,15 +1,19 @@
-import * as React from 'react';
-import { ThemeProvider as Provider, JssProvider } from 'react-jss';
+import React from 'react';
+import { ThemeProvider as JssThemeProvider, JssProvider } from 'react-jss';
 import PropTypes from 'prop-types';
 
-const ThemeProvider = ({ theme, children }) => (
-  <Provider theme={theme}>
-    <JssProvider
-      classNamePrefix="stargate-"
-      children={children}
-    />
-  </Provider>
-);
+const ThemeProvider = ({ theme, children }) => {
+  const minify = process.env.NODE_ENV === 'production';
+
+  return (
+    <JssProvider id={{minify}}>
+      <JssThemeProvider
+        theme={theme}
+        children={children}
+      />
+    </JssProvider>
+  );
+};
 
 ThemeProvider.propTypes = {
   children: PropTypes.node.isRequired,
