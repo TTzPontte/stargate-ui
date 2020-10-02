d501f3b0bd263fd1967f36942972fbdfaa9a720c
keitoliveira <keitoliveira@outlook.com>
2020-10-01 21:14:07 -0300

    fix(icons): add `Theme` from Stargate UI Styles

--- packages/stargate-ui-icons/src/utils/createIcon.js
+++ packages/stargate-ui-icons/src/utils/createIcon.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { SvgIcon } from '@pontte/stargate-ui-core';
+import { Theme } from '@pontte/stargate-ui-styles';
 
 const createIconComponent = (name, path) => (
   React.memo(
     React.forwardRef((props, ref) => (
-      <SvgIcon
-        ref={ref}
-        name={name}
-        children={path}
-        {...props}
-      />
+      <Theme>
+        <SvgIcon
+          ref={ref}
+          name={name}
+          children={path}
+          {...props}
+        />
+      </Theme>
     ))
   )
 );
