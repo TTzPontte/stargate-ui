9301f9dfe597e1cda04f10acbf8e486641230cff
keitoliveira <keitoliveira@outlook.com>
2020-10-29 01:46:59 -0300

    refactor(core): sort order of imports

--- packages/stargate-ui-core/src/index.js
+++ packages/stargate-ui-core/src/index.js
@@ -1,26 +1,26 @@
 export * from './icons';
 
+export { default as Accordion } from './Accordion';
+export * from './Accordion';
+
+export { default as Bar } from './Bar';
+export { default as Baseline } from './Baseline';
 export { default as Button } from './Button';
-export { default as Typography } from './Typography';
+export { default as ButtonAction } from './ButtonAction';
 export { default as Card } from './Card';
-export { default as Svg } from './Svg';
-export { default as SvgIcon } from './SvgIcon';
-export { default as Label } from './Label';
-export { default as Input } from './Input';
+export { default as Container } from './Container';
+export { default as Count } from './Count';
+export { default as Divider } from './Divider';
+export { default as Drawer } from './Drawer';
 export { default as Factory } from './Factory';
 export { default as Grid } from './Grid';
-export { default as Container } from './Container';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
 export { default as Link } from './Link';
-export { default as Divider } from './Divider';
-export { default as Bar } from './Bar';
-export { default as Drawer } from './Drawer';
-export { default as ButtonAction } from './ButtonAction';
-export { default as Baseline } from './Baseline';
 
 export { default as MenuList } from './MenuList';
 export * from './MenuList';
 
-export { default as Accordion } from './Accordion';
-export * from './Accordion';
-
-export { default as Count } from './Count';
+export { default as Svg } from './Svg';
+export { default as SvgIcon } from './SvgIcon';
+export { default as Typography } from './Typography';
